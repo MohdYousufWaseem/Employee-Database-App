137748b95e4ec5f1f7a0512905b6a961df29b681
MohdYousufWaseem <141504179+MohdYousufWaseem@users.noreply.github.com>
2025-08-14 21:58:01 +0530

    Update DBConnection.java

--- Employee Database App/src/main/java/org/example/DatabaseConnection/DBConnection.java
+++ Employee Database App/src/main/java/org/example/DatabaseConnection/DBConnection.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/company";
     private static final String USER = "root";
-    private static final String PASSWORD = "YOusuf12#";
+    private static final String PASSWORD = "Enter_Your_Password_here";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
